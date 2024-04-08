f977b63de0433f1f54440cea5cd49ff6a756c3f6
Serhoo <hlavatskyi.serhii@globaldev.tech>
2024-04-08 18:24:06 +0300

    Update sonic

--- sonic.py
+++ sonic.py
@@ -1,3 +1,14 @@
-
 def run():
     print("Sonic is running")
+
+
+def jump():
+    print("Sonic is jumping")
+
+
+def roll():
+    print("Sonic is rolling")
+
+
+def collect_ring():
+    print("Sonic is collecting ring")
